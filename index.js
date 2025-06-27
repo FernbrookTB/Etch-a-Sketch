1bd35731004c8a725fe0f9a8d100cfd18299e6e2
Sean <214511560+FernbrookTB@users.noreply.github.com>
2025-06-27 13:14:04 -0500

    Added grid to the page

---
+++ index.js
@@ -0,0 +1,7 @@
+let num = 256;
+for (i = 0; i < num; i ++) {
+    const createDiv = document.createElement('div');
+    document.getElementById(container);
+    container.appendChild(createDiv);
+}
+
