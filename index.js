ac3cb9fc1b0c884b2240bb51e2d0c329bedbdfc6
Sean <214511560+FernbrookTB@users.noreply.github.com>
2025-07-01 10:10:52 -0500

    Added the color button functionality and a hsl slider to chose color

--- index.js
+++ index.js
@@ -8,7 +8,39 @@ for (i = 0; i < num; i ++) {
 const squares = document.querySelectorAll('#container div');
 squares.forEach (container => {
     container.addEventListener ( 'mouseover', () => {
-        container.style.backgroundColor = 'black'
+        container.style.backgroundColor = isColorMode ? getCurrentColor() : 'black'
 
     });
+
+    
 })
+
+
+function getCurrentColor() {
+    const hue = document.getElementById('hueSlider').value;
+        return `hsl(${hue}, 100%, 50%)`;
+}
+
+const toggleButton = document.getElementById('color');
+const colorControl = document.getElementById('rgbControls');
+
+let isColorMode = false;
+
+toggleButton.addEventListener('click', () => { 
+    isColorMode = !isColorMode;
+    
+    document.body.style.backgroundColor = isColorMode ? '#f0f0f0' : 'white';
+    colorControl.style.display = isColorMode ? 'block' : 'none';
+});
+
+function erase() {
+    const lackOf = document.getElementById('eraser');
+
+let isErased = false;
+
+lackOf.addEventListener('click', () => {
+    isErased = !isErased;
+
+    
+} );
+}
